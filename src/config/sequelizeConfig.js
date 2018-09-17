33d9ad3e8c5a2475732fd454c2b2aeb5b571d172
kod2nd <colinongengkiat@gmail.com>
2018-09-17 15:24:58 +0800

    setup sequelize with postgres

---
+++ src/config/sequelizeConfig.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+const { DB_DATABASENAME, DB_USERNAME, DB_PASSWORD } = process.env;
+
+const sequelize = new Sequelize(DB_DATABASENAME, DB_USERNAME, DB_PASSWORD, {
+    host: "localhost",
+    dialect: "postgres"
+})
+
