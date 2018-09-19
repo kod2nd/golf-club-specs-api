4c2f05296ddbded604aee013a337b50850446f6e
kod2nd <colinongengkiat@gmail.com>
2018-09-20 02:36:04 +0800

    sslify

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const indexRouter = require('./src/routes/index');
 const userRouter = require('./src/routes/users');
+const enforce = require('express-sslify');
 
 const app = express()
 app.use(express.json())
+app.use(enforce.HTTPS({trustProtoHeader: true}))
 
 indexRouter(app)
 userRouter(app)
